ac9ae2ff7753c4b9ac2983d922f3b45e85767fab
Oleg Beltsev <zasod@MacBook-Air-Oleg.local>
2025-11-29 23:39:43 +0700

    empty next.config.js – 100% compatible

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbopack: true, // для Next.js 16
-  },
-  typescript: {
-    ignoreBuildErrors: true, // игнорируем TS-ошибки в сборке
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
